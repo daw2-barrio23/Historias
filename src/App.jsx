7b9078bff6ead75802a579bc0ad60cd29381f627
daw2-barrio23 <113451779+daw2-barrio23@users.noreply.github.com>
2024-05-14 18:36:57 +0000

    fin historia 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { burger } from '@lucide/lab';
 import Icono from './Icono';
 
 const App = () => {
-  return <div>
+  return <div className='flex'>
       <Camera color="red" size={48} />
       <Camera size={48} fill="red" />
       <Icon iconNode={burger} /> 

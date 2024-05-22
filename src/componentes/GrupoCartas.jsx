d5e550a882b4c91b62ab2dbe92803b06bbec1536
daw2-barrio23 <113451779+daw2-barrio23@users.noreply.github.com>
2024-05-22 18:25:32 +0000

    fin historia 2

--- src/componentes/GrupoCartas.jsx
+++ src/componentes/GrupoCartas.jsx
@@ -3,7 +3,7 @@ import { Carta } from './Carta';
 
 export function GrupoCartas() {
   return (
-    <div className="flex flex-wrap justify-center gap-4 mt-4">
+    <div className="flex flex-wrap justify-center gap-4 mt-4 ">
       <Carta />
       <Carta />
       <Carta />
